5f3e2f19f54b91d60f04c21042200f9d38bee582
Viccex <137243089+Viccex@users.noreply.github.com>
2023-07-15 17:41:49 -0600

    Se agrega el proyecto

---
+++ src/main/java/com/appventas/AplicacionVentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.appventas;
+
+/**
+ *
+ * @author cliente
+ */
+public class AplicacionVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
